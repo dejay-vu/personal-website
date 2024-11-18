f221f75bc74f6c954e781d235fb31f354ed4ae9a
Junhao Zhang <howiejayzh@gmail.com>
2024-11-18 00:02:14 +0000

    refactor: Update social media links in Social component

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -6,7 +6,7 @@ export default function Social() {
   return (
     <div className="flex flex-row items-center gap-3">
       <Link
-        href="https://github.com/howiejayz"
+        href="https://github.com/dejay-vu"
         target="_blank"
         color="foreground"
         aria-label="GitHub"
@@ -22,7 +22,7 @@ export default function Social() {
         <LinkedInIcon />
       </Link>
       <Link
-        href="https://instagram.com/howiejayz"
+        href="https://instagram.com/dejay_vu_"
         target="_blank"
         color="foreground"
         aria-label="Instagram"
